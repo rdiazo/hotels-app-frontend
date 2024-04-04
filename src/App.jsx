35e35b6ccd5caf356878f21bc5d37939f7a22390
Roberto D Diaz O <rddiazo75@gmail.com>
2024-04-04 06:09:29 -0400

    hotels app frontend v1.0.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import HomePage from '../src/Pages/HomePage'
+//import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
 import UnknownPages from './pages/UnknownPages'
 import HotelInfoPage from './pages/HotelInfoPage'
 import HeaderShared from './components/shared/HeaderShared'
 import ReservationsPage from './pages/ReservationsPage'
+import HomePage from './Pages/HomePage'
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
   <div>
     <HeaderShared />
     <Routes>
-      <Route path='/' element={<HomePage />} />
+      <Route path='/' element={<HomePagee />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/hotel/:id' element={<HotelInfoPage />} />
