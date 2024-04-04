c039a15cb34070399615c97df9438576d85a223b
Roberto D Diaz O <rddiazo75@gmail.com>
2024-04-04 06:23:19 -0400

    hotels app frontend v1.0.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-//import HomePage from './pages/HomePage'
-import RegisterPage from './pages/RegisterPage'
-import LoginPage from './pages/LoginPage'
-import UnknownPages from './pages/UnknownPages'
-import HotelInfoPage from './pages/HotelInfoPage'
-import HeaderShared from './components/shared/HeaderShared'
-import ReservationsPage from './pages/ReservationsPage'
 import HomePage from './Pages/HomePage'
+//import HomePage from './pages/HomePage'
+import RegisterPage from './Pages/RegisterPage'
+import LoginPage from './Pages/LoginPage'
+import UnknownPages from './Pages/UnknownPages'
+import HotelInfoPage from './Pages/HotelInfoPage'
+import HeaderShared from './components/shared/HeaderShared'
+import ReservationsPage from './Pages/ReservationsPage'
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
   <div>
     <HeaderShared />
     <Routes>
-      <Route path='/' element={<HomePagee />} />
+      <Route path='/' element={<HomePage />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/hotel/:id' element={<HotelInfoPage />} />
